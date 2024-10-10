9fbd3c2380fe51bcfc81269078025b6ab63aa7f2
B3Y0NDER <dine.terzi@epitech.digital>
2024-10-10 16:44:45 +0200

    Create ex_06.js

---
+++ ex_06/ex_06.js
@@ -0,0 +1,39 @@
+tsParticles.load("particles1", {
+particles: {
+number: { value: 50 },
+color: { value: "#F95738" },
+shape: { type: "circle" },
+size: { value: 5 },
+move: { speed: 2 }
+    }
+});
+
+tsParticles.load("particles2", {
+particles: {
+number: { value: 80 },
+color: { value: "#0D3B66" },
+shape: { type: "square" },
+size: { value: 10 },
+move: { speed: 1 }
+    }
+});
+
+tsParticles.load("particles3", {
+particles: {
+number: { value: 100 },
+color: { value: "#FAF0CA" },
+shape: { type: "triangle" },
+size: { value: 4 },
+move: { speed: 3, direction: "bottom" }
+    }
+});
+
+tsParticles.load("particles4", {
+    particles: {
+number: { value: 60 },
+color: { value: "#EE964B" },
+shape: { type: "polygon", polygon: { sides: 6 } },
+size: { value: 8 },
+move: { speed: 2, direction: "top" }
+}
+});
